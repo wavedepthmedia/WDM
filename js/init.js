c1597cc8098b11cfd824ce248c4cd6ab0cf4903c
wavedepthmedia <wavedepthmedia@mail.ru>
2025-05-24 23:24:21 +0300

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,32 @@
+import Preloader from './core/preloader.js';
+import MobileMenu from './core/mobile-menu.js';
+import { PartnersCarousel } from './modules/partners-carousel.js';
+import { MediaPlayer } from './modules/media-player.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Инициализация прелоадера
+  const preloader = new Preloader();
+  preloader.init();
+
+  // Мобильное меню
+  const mobileMenu = new MobileMenu();
+
+  // Карусель партнёров
+  const partnersCarousel = new PartnersCarousel('.partners-grid');
+
+  // Радио плеер
+  const radioPlayer = new MediaPlayer('#radio-player');
+
+  // Портфолио
+  const portfolioItems = document.querySelectorAll('.portfolio-item');
+  portfolioItems.forEach(item => {
+    item.addEventListener('click', () => {
+      // Логика открытия проекта
+    });
+  });
+});
+
+window.addEventListener('load', () => {
+  // Анимации после полной загрузки
+  document.body.classList.add('loaded');
+});
